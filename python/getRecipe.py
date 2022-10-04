cc5a011b683693e91d8be7c5331b51b9873d5f6c
chikiuto <abetokichiro@gmail.com>
2022-10-04 19:48:07 +0900

    create python file

---
+++ python/getRecipe.py
@@ -0,0 +1,90 @@
+# %%
+
+import json
+import time
+from pprint import pprint
+import requests
+import pandas as pd
+
+res = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=1038863537950891238')
+json_data = json.loads(res.text)
+
+category_columns = ['category1','category2','category3','categoryId','categoryName']
+category_df = pd.DataFrame(columns=category_columns)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# 大カテゴリ
+for category in json_data['result']['large']:
+    list_large = [[category['categoryId'],
+                              "",
+                              "",
+                              category['categoryId'],
+                              category['categoryName']
+                  ]]
+    df_append = pd.DataFrame(data=list_large, columns=category_columns)
+    category_df = pd.concat([category_df, df_append], ignore_index=True, axis=0)
+
+# 中カテゴリ
+parent_dict = {} # mediumカテゴリの親カテゴリの辞書
+for category in json_data['result']['medium']:
+    list_medium = [[category['parentCategoryId'],
+                    category['categoryId'],
+                    "",
+                    str(category['parentCategoryId'])+"-"+str(category['categoryId']),
+                    category['categoryName']
+                  ]]
+    df_append = pd.DataFrame(data=list_medium, columns=category_columns)
+    category_df = pd.concat([category_df, df_append], ignore_index=True, axis=0)
+    parent_dict[str(category['categoryId'])] = category['parentCategoryId']
+
+# 小カテゴリ
+for category in json_data['result']['small']:
+    list_small = [[ parent_dict[category['parentCategoryId']],
+                    category['parentCategoryId'],
+                    category['categoryId'],
+                    parent_dict[category['parentCategoryId']]+"-"+str(category['parentCategoryId'])+"-"+str(category['categoryId']),
+                    category['categoryName']
+              ]]
+    df_append = pd.DataFrame(data=list_small, columns=category_columns)
+    category_df = pd.concat([category_df, df_append], ignore_index=True, axis=0)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# キーワードを含む行を抽出
+category_df_keyword = category_df.query('categoryName.str.contains("サラダ")', engine='python')
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# 人気レシピを取得する
+# 取得したレシピはDataFrameに格納する
+recipe_columns = ['recipeId', 'recipeTitle', 'foodImageUrl', 'recipeMaterial', 'recipeCost', 'recipeIndication', 'categoryId', 'categoryName']
+recipe_df = pd.DataFrame(columns=recipe_columns)
+
+for index, row in category_df_keyword.iterrows():
+    time.sleep(1) # 連続でアクセスすると先方のサーバに負荷がかかるので少し待つのがマナー
+
+    url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1038863537950891238&categoryId='+row['categoryId']
+    res = requests.get(url)
+
+    json_data = json.loads(res.text)
+    recipes = json_data['result']
+
+    for recipe in recipes:
+        list_small = [[ recipe['recipeId'],
+                        recipe['recipeTitle'],
+                        recipe['foodImageUrl'],
+                        recipe['recipeMaterial'],
+                        recipe['recipeCost'],
+                        recipe['recipeIndication'],
+                        row['categoryId'],
+                        row['categoryName']
+                  ]]
+        df_append = pd.DataFrame(data=list_small, columns=recipe_columns)
+        recipe_df = pd.concat([recipe_df, df_append], ignore_index=True, axis=0)
+
+recipe_df_keyword = recipe_df.query('recipeIndication.str.contains("5分以内")', engine='python')
+recipe_df_keyword
+
+# %%
