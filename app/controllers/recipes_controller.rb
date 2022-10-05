01a229d085bb100f56ee8b4b3946f36337547315
chikiuto <abetokichiro@gmail.com>
2022-10-05 15:07:05 +0900

    edit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
   def index
+    @recipe = `/Library/Developer/CommandLineTools/usr/bin/python3 /Users/tokichi/abe_no_folder/medirom_Web/API_prac/python/getRecipe.py`
+    @hoge = 'hoge'
   end
 end
