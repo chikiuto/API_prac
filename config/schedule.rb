ece36694209d2790127db58524082258e3c68d68
chikiuto <abetokichiro@gmail.com>
2022-10-22 16:33:43 +0900

    comp whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,18 +18,32 @@
 # end
 # Learn more: http://github.com/javan/whenever
 
-# 【参考】https://qiita.com/Esfahan/items/e7a924f7078faf3294f2
+# whenever 定期実行
+# rake 'ファイル名':タスク名'
+
 # Rails.root(Railsメソッド)を使用するために必要
 require File.expand_path(File.dirname(__FILE__) + '/environment')
-# cronを実行する環境変数(:development, :product, :test)を定義
+
+# .zshrcとrbenvのパスを指定するrakeを定義
+# cronは.zshとrbenvの環境で動いてくれないためPATHを通す
+job_type :rake, "source /Users/tokichi/.zshrc; export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
+
+# cronを実行する環境変数(:development, :product, :test)
+# 環境変数'RAILS_ENV'にセットされている変数またはdevelopmentを指定
 rails_env = ENV['RAILS_ENV'] || :development
-# cronを実行する環境変数をセット(今回はdevelopment)
+
+# cronを実行する環境変数をセット
+# 今回はdevelopmentをセット
 set :environment, rails_env
+
 # cronのログの吐き出し場所
 set :output, "#{Rails.root}/log/cron.log"
 
+#/config/schedule.rb
+set :job_template, "/bin/zsh -l -c ':job'"
+job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
+
 # 毎日実行
-# every 1.day, at: '2:52 pm' do
-every 1.day, at: '3:15 pm' do
+every 1.day, at: '4:32 pm' do
   rake 'bat:update_category_table'
 end
