e6df08098a7f65cbdbd7958014a730f7d5fbe871
chikiuto <abetokichiro@gmail.com>
2022-11-05 17:05:36 +0900

    fix

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,5 @@
 class RecipesController < ApplicationController
   def index
-    # recipe_title
-    # recipe_url
-    # food_image_url
-    # recipe_material
-    # recipe_cost
-    # recipe_indication
-
     if params[:zairyou]
       @recipes = Recipe.where("material LIKE ?", "%#{params[:zairyou]}%")
       if params[:jikan]
